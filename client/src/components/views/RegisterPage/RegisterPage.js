f02e9ebfe986e6c8d31099cd585bf1112e6865fb
baobabnamu <iwantbaobab@gmail.com>
2021-09-07 00:39:19 +0900

    login page & logut func add

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { registerUser } from '../../../_actions/user_action'
 
-function RegisterPage() {
+function RegisterPage(props) {
+  const dispatch = useDispatch();
+
+  const [Email, setEmail] = useState("")  
+  const [Name, setName] = useState("")
+  const [Password, setPassword] = useState("")
+  const [ConfirmPassword, setConfirmPassword] = useState("")
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value) // state 실시간 값 입력
+  }
+
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value) // state 실시간 값 입력
+  }
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value) // state 실시간 값 입력
+  }
+
+  const onConfirmPasswordHandler = (event) => {
+    setConfirmPassword(event.currentTarget.value) // state 실시간 값 입력
+  }
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault(); // 페이지 새로고침 방지
+
+    if(Password !== ConfirmPassword) {
+      return alert('입력하신 비밀번호가 일치하지 않습니다.')
+    }
+    
+    let body = {
+      email: Email,
+      password: Password,
+      name: Name
+    }
+
+    dispatch(registerUser(body))
+      .then(response => {
+        if(response.payload.success) {
+          props.history.push("/login")
+        } else {
+          alert("Failed to Sign Up!!")
+        }
+      })
+  }
   return (
-    <div>
-      RegisterPage
+    <div style={{
+      display:'flex', justifyContent:'center', alignItems: 'center'
+      , width: '100%', height: '100vh'
+    }}>
+      <form style={{display:'flex', flexDirection:'column'}}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Name</label>
+        <input type="text" value={Name} onChange={onNameHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <label>ConfirmPassword</label>
+        <input type="password" value={ConfirmPassword} onChange={onConfirmPasswordHandler} />
+        <br/>
+        <button>
+          회원 가입
+        </button>
+      </form>
     </div>
   )
 }
